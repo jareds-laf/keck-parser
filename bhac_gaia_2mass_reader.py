895855fb66b887549534820847cc8b61677f8d6f
jareds-laf <sofairj@lafayette.edu>
2022-06-23 13:19:23 -0400

    (Mostly) QoL updates
    Cleaned up the code of bhac_gaia_2mass_reader.py
    
    Added preliminary plot of absolute G mag vs. BP-RP model based on BHAC table for t = 0.8000 Gyr

--- bhac_gaia_2mass_reader.py
+++ bhac_gaia_2mass_reader.py
@@ -12,13 +12,10 @@ import os
 import astropy.io.ascii as at
 import linecache as lc
 
+
 # Path of files
 input_path = os.path.expanduser(r"G:/Shared drives/DouglasGroup/models/BHAC15")
 output_path = os.path.expanduser(r"G:/Shared drives/DouglasGroup/Jared Sofair 2022/CSV Files/BHAC Tables")
-# Column names
-# names_tmass = ["M/Ms", "Teff", "L/Ls", "g", "R/Rs", "Li/Li0", "Mj", "Mh", "Mk"]
-# names_gaia = ["M/Ms", "Teff", "L/Ls", "g", "R/Rs", "Li/Li0", ""]
-
 
 # Try numpy loadtext
 input_file_tmass = os.path.join(input_path, r"BHAC15_iso.2mass")
@@ -26,12 +23,9 @@ input_file_gaia = os.path.join(input_path, r"BHAC15_iso.GAIA")
 
 bhac_tmass = os.path.join(output_path, r"BHAC_2MASS.csv")
 bhac_gaia = os.path.join(output_path, r"BHAC_GAIA.csv")
-# Read in tables
-# tmass_initial = at.read(tmass_file, comment='!', data_start=759, data_end=782, format='no_header', guess=False, fast_reader=False, names=names_tmass)#, names = names_tmass) # Can't start var name with a # :(
-# gaia = at.read(os.path.join(bhac_path, r'BHAC15_iso.GAIA'), data_start=821, data_end=844, format='basic', delimiter=' ', guess=False, fast_reader=False, names = names_gaia)
-# print(tmass_initial)
 
-# 2MASS
+
+#%% 2MASS
 # Reformat the table
 age_tmass = "0.8000"
 data_tmass = Table()
@@ -63,7 +57,7 @@ with open(input_file_tmass, 'r') as f_tmass:
     # Write data to csv :D
     data_tmass.write(bhac_tmass, overwrite=True)
 
-# GAIA
+#%% GAIA
 # Reformat the table
 age_gaia = "0.8000"
 data_gaia = Table()
@@ -94,22 +88,14 @@ with open(input_file_gaia, 'r') as f_gaia:
     
     # Write data to csv :D
     data_gaia.write(bhac_gaia, overwrite=True)
-
-# split the rows as well and save them as an array/table
-
-# Put the age as a column, read the table and export it as a csv, use the csv in the actual code
-
-# Read the line, split it, tack on the age column
+#%%
 
 # Can also put it in Excel and reformat it there
 
-# loop through the actual data, add a comma between each entry
-# tack on age at the end
 
-# Get the length of the file
-# with open(tmass_file, 'r') as f:
-#     print(f.readlines())
-#     length = len(f.readlines())
-
+# Read in tables
+# tmass_initial = at.read(tmass_file, comment='!', data_start=759, data_end=782, format='no_header', guess=False, fast_reader=False, names=names_tmass)#, names = names_tmass) # Can't start var name with a # :(
+# gaia = at.read(os.path.join(bhac_path, r'BHAC15_iso.GAIA'), data_start=821, data_end=844, format='basic', delimiter=' ', guess=False, fast_reader=False, names = names_gaia)
+# print(tmass_initial)
     
     # lc might open and close the file everytime you open it? if so, just use f.readline
--- praesepe_cmd_mass.py
+++ praesepe_cmd_mass.py
@@ -4,26 +4,42 @@ Created on Wed Jun 22 11:36:18 2022
 
 @author: Jared
 """
-
 import numpy as np
 import matplotlib.pyplot as plt 
 from astropy.table import Table, Column
+import os
 
-# Read Praesepe_Merged.csv
-pm = Table.read(r'C:\Users\Jared\Desktop\EXCEL Summer 2022\Catalogs\Praesepe_Merged.csv')
+# Read in necessasry csv files
+csv_path = r"G:/Shared drives/DouglasGroup/Jared Sofair 2022/CSV Files"
+pm = Table.read(os.path.join(csv_path, r'Praesepe_Merged.csv'))
+bhac_gaia = Table.read(os.path.join(csv_path, r"BHAC Tables/BHAC_GAIA.csv"))
+bhac_2mass = Table.read(os.path.join(csv_path, r"BHAC Tables/BHAC_2MASS.csv"))
 
-# Creating G-K and BP-RP columns
-GminusK = Column(data=(pm['G']-pm['K']))
-BPminusRP = Column(data=(pm['BP']-pm['RP']))
 
-pm.add_column(GminusK, name = 'G-K')
-pm.add_column(BPminusRP, name = 'BP-RP')
+# Creating color index columns
+GminusK_pm = Column(data=(pm['G']-pm['K']))
+BPminusRP_pm = Column(data=(pm['BP']-pm['RP']))
+pm.add_column(GminusK_pm, name = 'G-K')
+pm.add_column(BPminusRP_pm, name = 'BP-RP')
+
+
+absG_gaia = Column(data=bhac_gaia['G']) # Currently appg, not absg
+BPminusRP_gaia = Column(data=bhac_gaia['G_BP']-bhac_gaia['G_RP'])
+bhac_gaia.add_column(BPminusRP_gaia, name = 'BP-RP')
+
+
+# absK_2mass = Column(data=bhac_2mass['Mk']) # Currently appk, not absk
+# JminusK_2mass = Column(data=bhac_2mass['Mj']-bhac_2mass['Mk'])
+# bhac_2mass.add_column(JminusK_2mass, name = 'J-K')
 
 # Creating column with absolute magnitudes
-absG = pm['G'] - 5*np.log(pm['D']) + 5    # absmag = appmag - 5*log(D) + 5
-pm.add_column(absG, name='absG')
+absG_pm = pm['G'] - 5*np.log(pm['D']) + 5    # absmag = appmag - 5*log(D) + 5
+pm.add_column(absG_pm, name='absG')
 
-# Removing non-main-sequence stars
+absG_gaia = bhac_gaia['G']
+bhac_gaia.add_column(absG_gaia, name = 'absG')
+
+#%% Removing non-main-sequence stars
 
 # Bad ranges
 low1 = pm['BP-RP'] <= 0.0
@@ -41,13 +57,31 @@ high4 = pm['absG'] <= -7.5
 low5 = pm['G-K'] <= 6.0
 high5 = pm['absG'] >= -1.0
 
-bad = (high1 & low1) | (high2 & low2) | (high3 & low3) | (low5 & high5)
+low6 = pm['G-K'] >= 3.0
+high6 = pm['absG'] <= -12.0
+
+low7 = pm['G-K'] <= 1.0
+high7 = pm['absG'] <= -14.0
+
+low8 = pm['G-K'] >= 2.6
+high8 = pm['absG'] <= -7.8
+
+low9 = pm['BP-RP'] >= 1.0
+high9 = pm['absG'] <= -14.0
+
+low10 = pm['BP-RP'] >= 1.0
+high10 = pm['absG'] <= -14.0
+
+low11 = pm['BP-RP'] <= 2.6
+high11 = pm['absG'] >= -4.0
+
+bad = (high1 & low1) | (high2 & low2) | (high3 & low3) | (low5 & high5) | (low6 & high6) | (low7 & high7) | (low8 & high8) | (low9 & high9) | (low10 & high10) | (low11 & high11)
 # print(np.where(bad)[0])
 pm.remove_rows(np.where(bad)[0])
 
 # Creating bins by color index
 
-# Setting bins
+#%% Setting bins
 color_bin1 = np.trunc(pm['G-K'] / 0.5) 
 color_bin2 = np.trunc(pm['BP-RP'] / 0.5)
 
@@ -58,37 +92,37 @@ pm_binned_color1 = pm_grouped_color1.groups.aggregate(np.mean)
 pm_binned_color2 = pm_grouped_color2.groups.aggregate(np.mean)
 
 # Setting a bin to a different color
-low_red1 = pm['absG'] >= pm_binned_color1['absG'][4]
-high_red1 = pm['absG'] <= pm_binned_color1['absG'][5]
+low_bin1 = pm['absG'] >= pm_binned_color1['absG'][4]
+high_bin1 = pm['absG'] <= pm_binned_color1['absG'][5]
 
-color_r = low_red1 & high_red1
-# Plotting G vs. G-K
+color_bin1 = low_bin1 & high_bin1
+#%% Plotting G vs. G-K
 
-# Creating 1st plot
-fig1, ax1 = plt.subplots()
-ax1.plot(pm['G-K'], pm['absG'], '.', color='#5fa2fa')
-ax1.plot(pm['G-K'][color_r], pm['absG'][color_r], '.', color='red')
+# # Creating 1st plot
+# fig1, ax1 = plt.subplots()
+# ax1.plot(pm['G-K'], pm['absG'], '.', color='#5fa2fa')
+# ax1.plot(pm['G-K'][color_bin1], pm['absG'][color_bin1], '.', color='green')
 
-ax1.plot(pm_binned_color1['G-K'], pm_binned_color1['absG'], 'o', color='orange')
-# ax1.plot(pm_binned_color1['G-K'][color_r], pm_binned_color1['absG'][color_r], 'o', color='red')
-# ax1.plot(pm['G-K'][bad], pm['absG'][bad], 'o', color='red') # Uncomment to see removed data
+# ax1.plot(pm_binned_color1['G-K'], pm_binned_color1['absG'], 'o', color='orange')
+# # ax1.plot(pm_binned_color1['G-K'][color_bin1], pm_binned_color1['absG'][color_bin1], 'o', color='red')
+# # ax1.plot(pm['G-K'][bad], pm['absG'][bad], 'o', color='red') # Uncomment to see removed data
 
-plt.title("G vs. (G-K)")
-ax1.set_xlabel('(G-K)')
-ax1.set_ylabel('Absolute G Magnitude')
-ax1.invert_yaxis()
+# plt.title("G vs. (G-K)")
+# ax1.set_xlabel('(G-K)')
+# ax1.set_ylabel('Absolute G Magnitude')
+# ax1.invert_yaxis()
 
 
 
-# Plotting G vs. BP-RP
+#%% Plotting G vs. BP-RP
 
-    # Creating 2nd plot
+# Creating 2nd plot
 fig2, ax2 = plt.subplots()
 ax2.plot(pm['BP-RP'], pm['absG'], '.', color='#695ffa')
 
 ax2.plot(pm_binned_color2['BP-RP'], pm_binned_color2['absG'], 'o', color='orange')
-ax2.plot(pm['BP-RP'][color_r], pm['absG'][color_r], '.', color='red')
-
+ax2.plot(pm['BP-RP'][color_bin1], pm['absG'][color_bin1], '.', color='green')
+ax2.plot(bhac_gaia['BP-RP'], bhac_gaia['absG'], '.', color='pink')
 # ax2.plot(pm['BP-RP'][bad], pm['absG'][bad], 'o', color='red') # Uncomment to see removed data
 
 plt.title("G vs. (BP-RP)")
